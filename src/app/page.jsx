e5ef9b4bee8c83a6c4aa670383ab579adac7d018
Vitor Dallanol <125999178+Crautor@users.noreply.github.com>
2024-05-17 11:20:21 -0300

    feat: about me & responsiveness & favicon

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 // Components
 import InitialScreen from '@/components/InitialScreen';
 import LanguageCarousel from '@/components/languageCarousel';
+import AboutMe from '@/components/aboutMe';
 
 const Home = () => {
   return (
     <div className='h-screen w-full bg-neutral-200'>
       <InitialScreen />
       <LanguageCarousel />
+      <AboutMe />
     </div>
   );
 };
---
+++ src/utils/aboutArray.js
@@ -0,0 +1,27 @@
+export const aboutWork = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'TailWind CSS',
+  'NextJS',
+  'NodeJS',
+  'Express',
+  'MongoDB',
+  'GitHub',
+];
+
+export const aboutCollege = [
+  'C',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Java',
+  'SpringBoot',
+  'MySQL',
+  'GitLab',
+  'GitHub',
+  'Arduino',
+  'Docker',
+  'AWS',
+];
