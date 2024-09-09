21c26175925c8c6a121625a97ba547be778e32e5
Crautor <vitor.dallanol@gmail.com>
2024-09-09 09:52:00 -0300

    feat: new job

--- src/components/experiencesCard/index.jsx
+++ src/components/experiencesCard/index.jsx
@@ -3,17 +3,28 @@ import React from 'react';
 // Utils
 import spentTime from '@/utils/spentTime';
 
-const experiencesCard = ({ job, company, startAt, endAt, description, current }) => {
+const experiencesCard = ({
+  job,
+  company,
+  startAt,
+  endAt,
+  description,
+  current,
+}) => {
   return (
-    <div className='flex flex-col shadow-2xl p-5 hover:bg-white/80 hover:scale-105 duration-500 rounded-'>
+    <div className='flex flex-col shadow-2xl p-5 w-[80%] hover:bg-white/80 hover:scale-105 duration-500 rounded-'>
       <p className='text-xl font-bold'>{job}</p>
       <p>{company}</p>
       <p className='text-gray-500 text-xs'>
-        {startAt} - {current ? 'Present' : endAt} · {spentTime(startAt, endAt, current)} 
+        {startAt} - {current ? 'Present' : endAt} ·{' '}
+        {spentTime(startAt, endAt, current)}
       </p>
       <ul>
         {description.map((item, index) => (
-          <div key={index} className='flex justify-start items-center gap-2 w-full h-auto '>
+          <div
+            key={index}
+            className='flex justify-start items-center gap-2 w-full h-auto '
+          >
             {/* dot */}
             <span className='bg-emerald-400 w-2 h-2 rounded-full flex'></span>
             <li className='w-11/12 h-auto '>{item}</li>
--- src/utils/experiencesArray.js
+++ src/utils/experiencesArray.js
@@ -3,12 +3,22 @@ export const experiencesArray = [
     job: 'Full-Stack Developer',
     company: 'Global Transportes',
     startAt: '2023-12-01',
-    endAt: '-',
+    endAt: '2024-09-01',
     description: [
       "I implemented new features and fixed bugs in the company's website.",
-      'Used technologies: NextJS, ReactJS, Tailwind CSS, NodeJS, JavaScript, Express, MongoDB, GitHub',
+      'Used technologies: NextJS, ReactJS, Tailwind CSS, NodeJS, JavaScript, Express, MongoDB, MySQL, GitHub.',
+    ],
+    current: false,
+  },
+  {
+    job: 'Developer',
+    company: 'SmartFlow Sistemas',
+    startAt: '2024-09-02',
+    endAt: '-',
+    description: [
+      'I am currently working on the development of monitoring systems for the company.',
+      'Used technologies: Python, EMQX, TDengine, GitLab.',
     ],
     current: true,
   },
-
 ].sort((a, b) => new Date(b.startAt) - new Date(a.startAt));
